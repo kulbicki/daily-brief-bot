d32788d6ad0f01f97acd5614006aed27beca1a27
Aliaksej <lelika@bk.ru>
2026-03-24 22:31:54 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+text = """🌍 Кароткі дайджэст
+
+— Тэставае паведамленне. Бот працуе.
+"""
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+requests.post(url, data={"chat_id": CHAT_ID, "text": text})
